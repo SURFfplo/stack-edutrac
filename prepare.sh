1464504a2ef08aa5ddd344d9fa627032513e7c04
Herman <herman@dompseler.nl>
2019-04-18 16:27:45 +0200

    move docroot

--- prepare.sh
+++ prepare.sh
@@ -6,5 +6,5 @@ mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/data/mysql
 mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/src
 
 # write data
-cp -a ./src/* /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/html
+cp -a ./src/$STACK_SERVICE/* /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/html
 #cp -a ./src/* /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/src
